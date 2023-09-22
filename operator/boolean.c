bc318c79ba2507ecc53f68dd57cd093320342585
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-09-22 09:04:12 +0530

    Add files via upload
    opertator

---
+++ operator/boolean.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdbool.h>
+
+ main()
+ {
+     bool a = true;
+     printf(" a== %d",a);
+
+     int x=10, y=10;
+
+    printf("\n a == b  = %d",x==y);
+    printf("\n a != b  = %d",x!=y);
+    printf("\n a <  b  = %d",x<y);
+    printf("\n a <= b  = %d",x<=y);
+    printf("\n a >  b  = %d",x>y);
+    printf("\n a >= b  = %d",x>=y);
+
+ }
+
