8717f711ba4b0aa773bc49149977971294a056d1
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-09-29 09:01:50 +0530

    Add files via upload

---
+++ type_conversion/Implicit_Type_Conversion.c
@@ -0,0 +1,13 @@
+//Implicit Type Conversion char to int
+
+#include<stdio.h>
+
+int main() {
+
+  char alphabet = 'a';
+
+  int number = alphabet;
+  printf("Integer Value 'a' is : %d", number);
+
+  return 0;
+}
